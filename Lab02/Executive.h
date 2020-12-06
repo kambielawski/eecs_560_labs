98c367bfc6579e9a333a1f7af574a11bcf098c4c
Kam Bielawski <kamtb28@gmail.com>
2020-12-05 18:41:57 -0600

    read entries into hashtable

--- Lab02/Executive.h
+++ Lab02/Executive.h
@@ -2,12 +2,18 @@
 #define EXECUTIVE_H
 
 #include <string>
+#include <stdexcept>
+#include <algorithm>
 #include <iostream>
+#include <fstream>
+#include "HashTable.h"
 using namespace std;
 
 class Executive {
 private:
 ifstream &m_infile;
+HashTable<int> m_hashtable;
+void readFileIntoHashTable(ifstream& infile);
 
 public:
 Executive(ifstream& infile);
@@ -17,16 +23,33 @@ void run();
 };
 
 Executive::Executive(ifstream& infile) : m_infile(infile) {
-  string word;
-  while (m_infile >> word) {
-    cout << word << endl;
-  }
+  this->readFileIntoHashTable(m_infile);
 }
 
 Executive::~Executive() {}
 
 void Executive::run() {
   cout << "running!" << endl;
+}
+
+void Executive::readFileIntoHashTable(ifstream& infile) {
+  string entry;
+  string numGoalsStr;
+  int numGoals;
+  while (!infile.eof()) {
+    getline(infile, entry, ':');
+    getline(infile, numGoals, ',');
+    // remove whitespace
+    entry.erase(remove(entry.begin(), entry.end(), ' '), entry.end());
+    numGoalsStr.erase(remove(numGoalsStr.begin(), numGoalsStr.end(), ' '), numGoalsStr.end());
+    // convert numGoalsStr to int
+    try {
+      numGoals = stoi(numGoalsStr);
+    } catch (exception& e) {
+      throw e;
+    }
+    m_hashtable.add(numGoals, entry);
+  }
 }
 
 #endif
